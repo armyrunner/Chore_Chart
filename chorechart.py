6b8de5836d687c12afbf4679b99db60cefdbc3f0
Andrew <andrew.c.nelson@tcnbroadcasting.com>
2025-03-25 17:00:07 -0600

    update

--- chorechart.py
+++ chorechart.py
@@ -4,7 +4,7 @@ class ChoreChart:
 
     def __init__(self):
         self.chores = []
-        self.family_members = {}
+        self.family_members = []
         self.load_chores()
         self.load_family_members()
 
@@ -14,8 +14,13 @@ class ChoreChart:
             with open(filename,'r') as f:
                 self.chores = [line.strip() for line in f.readlines()]
 
-        execpt FileNotFoundError:
-            print("No File Found, Please add a chores file!!)
+        except FileNotFoundError:
+            print("No File Found, Please add a chores file!!")
+    
+
+    def load_family_members(self,name):
+        try:
+            with open(filename)
     
 
 
